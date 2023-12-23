8fb05611ab3bd761a93a7d46fb1a151516bbfef2
wesleybertipaglia <wesleybertipaglia@gmail.com>
2023-12-23 16:10:45 -0300

    refactor(logs): if path dont exists mkdir

--- helpers/Log.js
+++ helpers/Log.js
@@ -1,8 +1,7 @@
-const fs = require('fs');
-
 class Log {
     constructor(fileName) {
-        this.fs = fs;
+        this.fs = require('fs');
+        this.path = require('path');
         this.fileName = fileName;
     }
 
@@ -17,6 +16,11 @@ class Log {
     }
 
     Write(content) {
+        const pathToFile = this.path.dirname(this.fileName);
+        if (!this.fs.existsSync(pathToFile)) {
+            this.fs.mkdirSync(pathToFile);
+        }
+
         this.fs.writeFile(this.fileName, content, (err) => {
             if (err) {
                 console.error(err);
@@ -26,14 +30,8 @@ class Log {
         });
     }
 
-    Clear(content) {
-        this.fs.writeFile(this.fileName, "", (err) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            console.log('File cleared successfully');
-        });
+    Clear() {
+        this.Write("");
     }
 }
 
