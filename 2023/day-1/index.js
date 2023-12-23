1a5a121dc85b7ab4ea6b8f88b4fd7b3cb3b6628b
wesleybertipaglia <wesleybertipaglia@gmail.com>
2023-12-23 10:14:40 -0300

    chore: restructure the project

--- 2023/day-1/index.js
+++ 2023/day-1/index.js
@@ -1,8 +1,8 @@
 const input = require("./assets/input.js");
 const lines = input.split("\n");
-let totalOfCalibrationValues = 0;
 
 function partOne() {
+    let totalOfCalibrationValues = 0;
     lines.forEach(line => {
         let numbers = line.replace(/[^\d.]/g, '').split("").map((num) => num);
         let calibrationValue = parseInt(numbers[0] + numbers.pop());
